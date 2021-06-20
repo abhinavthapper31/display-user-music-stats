c41734ac305f1b63adcfba97c37922cc1de35b5d
Abhinav Thapper <abhinav.thapper3@gmail.com>
2021-06-20 18:27:41 +0530

    Initial Commits

---
+++ src/main/java/com/musicstats/displayuserstats/DisplayUserStatsApplication.java
@@ -0,0 +1,13 @@
+package com.musicstats.displayuserstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisplayUserStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisplayUserStatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musicstats/displayuserstats/DisplayUserStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicstats.displayuserstats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisplayUserStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
